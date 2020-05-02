31b1e5e32fcf68505968a0548ac8d650ae6aaeae
Shannon Trainor <sltrainor@outlook.com>
2020-05-02 11:45:06 -0400

    added updateHour function with if/else

--- script.js
+++ script.js
@@ -11,7 +11,35 @@ $(document).readt(function (){              //will allow js to load fully before
         localStorage.setItem(time, value);
     });
 
-    
+    // 
+    function updateHour () {
+        //check current hour
+        var currentHour = moment().hours();
+        console.log(currentHour);
+        $(".time-block").each(function(){
+                //turn into an array, will compare to current hour to determine if time is past/present/future
+            var blockHour = $(this).attr("id").split("-")[1];
+                //parse string & turn into interger
+            var newBlockHour = parseInt(blockHour);
+                //console.log to check
+            console.log(newBlockHour);
+
+
+            if (newBlockHour < currentHour){
+                // if new hour is smaller interger than the current hour, we need to add a class of past
+                $(this).addClass("past");
+            }
+            else if (newBlockHour === currentHour){
+                    //if current hour is the same as the new hour, we need to remove the past class so we can add present
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } 
+            else {      //if current hour is not past or present, assign class of future
+                $(this).addClass("future");
+            }
+        })
+    }
+    updateHour();       //call function
 
 
 
